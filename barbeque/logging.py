68b3eaa16cca0621773941f662bdfb888725a577
Christopher Grebs <cg@webshox.org>
2014-11-21 14:03:06 +0100

    Remove enable_error_logging_in_debug_mode helper, unused

--- barbeque/logging.py
+++ barbeque/logging.py
@@ -75,32 +75,3 @@ def logged(obj):
     """
     obj.logger = get_logger(obj)
     return obj
-
-
-def enable_error_logging_in_debug_mode():
-    """
-    If DEBUG = True then monkey patch the default DEBUG 500 response, so
-    that we also log the errors. (So that we can see them retrospectively).
-    """
-    from django.conf import settings
-
-    if settings.DEBUG:
-        from django.views import debug
-        import logging
-
-        orig_technical_500_response = debug.technical_500_response
-
-        def custom_technical_500_response(request, exc_type, exc_value, tb):
-            """
-            Create a technical server error response. The last three arguments are
-            the values returned from sys.exc_info() and friends.
-            """
-            logger = logging.getLogger('django.request')
-            logger.error(
-                u'Internal Server Error: %s' % request.path,
-                exc_info=(exc_type, exc_value, tb),
-                extra={'status_code': 500, 'request': request}
-            )
-            return orig_technical_500_response(request, exc_type, exc_value, tb)
-
-        debug.technical_500_response = custom_technical_500_response
