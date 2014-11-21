fdd8fd58a55299102f03ae36a62f10fd317a6a90
Christopher Grebs <cg@webshox.org>
2014-11-21 12:24:55 +0100

    Remove get_random_name

--- barbeque/utils/tests.py
+++ barbeque/utils/tests.py
@@ -1,10 +1,8 @@
-import random
 import pkg_resources
 
 import factory
 from django.contrib.messages.storage.cookie import CookieStorage
 from django.http import HttpRequest
-from django.utils.six.moves import xrange
 
 
 def get_messages_from_cookie(cookies):
@@ -12,29 +10,6 @@ def get_messages_from_cookie(cookies):
     request.COOKIES = {CookieStorage.cookie_name: cookies.get(
         CookieStorage.cookie_name).value}
     return CookieStorage(request)
-
-
-def get_random_name(min=4, max=6):
-    """This function returns a pronounceable word."""
-    consonants = u'bcdfghjklmnprstvwz'
-    vowels = u'aeiou'
-    numbers = u'0123456789'
-    all = consonants + vowels
-
-    length = random.randrange(min, (max - 2) / 2)
-
-    def _get_name():
-        return u''.join(
-            random.choice(consonants) +
-            random.choice(vowels) +
-            random.choice(all) for x in xrange(length // 3)
-        )[:length]
-
-    name1 = _get_name()
-    name2 = ''
-    if random.randint(1, 2) == 1:
-        name2 = _get_name() + random.choice(numbers + u'  ! ?   ')
-    return '{0} {1}'.format(name1.capitalize(), name2.capitalize()).strip()
 
 
 class SvgField(factory.django.FileField):
