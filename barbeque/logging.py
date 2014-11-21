98b93636a6ad0a952d4483109fb226b36d89751c
Christopher Grebs <cg@webshox.org>
2014-11-21 13:49:01 +0100

    Don't automatically enable error logging

--- barbeque/logging.py
+++ barbeque/logging.py
@@ -96,7 +96,3 @@ def enable_error_logging_in_debug_mode():
             return orig_technical_500_response(request, exc_type, exc_value, tb)
 
         debug.technical_500_response = custom_technical_500_response
-
-
-# TODO: is there a better init point for this?
-enable_error_logging_in_debug_mode()
