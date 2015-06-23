7786cd9cc0b0d23ac20a00fb4c90fda2633adfff
Stephan Jaekel <steph@rdev.info>
2015-06-23 17:35:47 +0200

    Added starspan filter.

--- barbeque/templatetags/barbeque_tags.py
+++ barbeque/templatetags/barbeque_tags.py
@@ -1,7 +1,15 @@
+import re
+
 from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
+
+
+STARSPAN_RE = re.compile(r'(\*\*\*)(.+?)\1')
 
 
 @register.simple_tag(name='set', takes_context=True)
@@ -10,3 +18,9 @@ def set_tag(context, **kwargs):
     for key, value in kwargs.items():
         context[key] = value
     return ''
+
+
+@register.filter
+@stringfilter
+def starspan(value):
+    return mark_safe(STARSPAN_RE.sub(r'<span>\2</span>', conditional_escape(value)))
--- barbeque/tests/test_templatetags.py
+++ barbeque/tests/test_templatetags.py
@@ -1,5 +1,7 @@
 from django.template import Context, Template
 
+from barbeque.templatetags.barbeque_tags import starspan
+
 
 class TestTemplateTags:
 
@@ -11,3 +13,15 @@ class TestTemplateTags:
 
         assert 'test_var' in context
         assert context['test_var'] == 'Some data'
+
+    def test_starspan_no_blocks(self):
+        assert starspan('test * **foo bar*** lorem') == (
+            'test * **foo bar*** lorem')
+
+    def test_starspan_multiple_blocks(self):
+        assert starspan('***asd foo** bar*** lorem ipsum *** dolor sit***') == (
+            '<span>asd foo** bar</span> lorem ipsum <span> dolor sit</span>')
+
+    def test_starspan_unbalanced_blocks(self):
+        assert starspan('asd foo** bar*** lorem ipsum *** dolor sit***') == (
+            'asd foo** bar<span> lorem ipsum </span> dolor sit***')
