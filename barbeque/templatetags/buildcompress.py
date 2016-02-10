98bee17bc7791b3b097824abf6826266cd6e3c79
Stephan Jaekel <steph@rdev.info>
2016-02-10 16:14:02 +0100

    Added buildcompress tag to allow production only compress tags.

---
+++ barbeque/templatetags/buildcompress.py
@@ -0,0 +1,30 @@
+from compressor.templatetags.compress import CompressorNode, OUTPUT_FILE
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+
+class BuildCompressNoopNode(template.Node):
+
+    def render(self, context):
+        return ''
+
+
+@register.tag
+def buildcompress(parser, token):
+    """
+    This is a slightly duplicated version of the compressor compress tag.
+    We need this tag to have a no-op output if DEBUG is True. This version
+    doesn't support anything else than the "kind" option (js/css).
+    """
+    nodelist = parser.parse(('endbuildcompress',))
+    parser.delete_first_token()
+    args = token.split_contents()
+    assert len(args) == 2, 'Invalid arguments to buildcompress.'
+
+    if settings.DEBUG:
+        return BuildCompressNoopNode()
+
+    return CompressorNode(nodelist, args[1], OUTPUT_FILE, None)
