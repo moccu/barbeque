87ce7d6fb5ae48da2905afa9ad851ea719873122
Magdalena Rother <magdalena.rother@moccu.com>
2016-09-25 20:54:00 +0200

    Add middleware for handling static files
    This code was developed in loreal-dessange-website project
    and copied here to make it available also in other projects.

---
+++ barbeque/static.py
@@ -0,0 +1,37 @@
+import re
+
+from django.conf import settings
+from django.http.response import Http404
+from django.views.static import serve
+
+
+class StaticFileMiddleware(object):
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        self.path_regex = re.compile(
+            r'^/{0}(.*)$'.format(settings.STATIC_URL.strip('/')))
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return self.process_response(request, response)
+
+    def process_response(self, request, response):
+        if not is_static_request(request, response):
+            return response
+        path = self.path_regex.match(request.path)
+        if not path:
+            return response
+        try:
+            response = serve(
+                request, path.group(1), document_root=settings.STATIC_ROOT)
+        except Http404:
+            pass
+        return response
+
+
+def is_static_request(request, response):
+    return all([
+        request.path.startswith(settings.STATIC_URL),
+        response.status_code in [301, 404],
+    ])
---
+++ barbeque/tests/test_static.py
@@ -0,0 +1,84 @@
+import os
+from unittest.mock import Mock, patch
+
+import pytest
+from django.http import HttpResponseNotFound, HttpResponsePermanentRedirect
+
+from barbeque.static import StaticFileMiddleware
+
+
+@pytest.fixture
+def patch_settings(settings):
+    static_root = os.path.join(
+        settings.ROOT_DIR, 'src', 'testing', 'resources', 'static')
+    settings.STATIC_ROOT = static_root
+
+
+def test_file_exists(rf, db, patch_settings):
+    request = rf.get('/static/test.jpg')
+    middleware = StaticFileMiddleware()
+    response = middleware.process_response(request, HttpResponseNotFound(''))
+    assert response.status_code == 200
+    assert response['Content-Type'] == 'image/jpeg'
+    assert len(response.items()) == 3
+    assert response.has_header('Content-Length')
+    assert response.has_header('Last-Modified')
+
+
+def test_file_missing(rf, patch_settings):
+    request = rf.get('/static/doesnotexist.jpg')
+    middleware = StaticFileMiddleware()
+    response = middleware.process_response(request, HttpResponseNotFound(''))
+    assert response.status_code == 404
+
+
+def test_unknown_prefix(rf, patch_settings):
+    request = rf.get('/foo/test.jpg')
+    middleware = StaticFileMiddleware()
+    response = middleware.process_response(request, HttpResponseNotFound(''))
+    assert response.status_code == 404
+
+
+def test_redirect_for_static(rf, db, patch_settings):
+    request = rf.get('/static/test.jpg')
+    middleware = StaticFileMiddleware()
+    response = middleware.process_response(
+        request, HttpResponsePermanentRedirect('/static/test.jpg/'))
+    assert response.status_code == 200
+
+
+def test_redirect_other(rf, db, patch_settings):
+    request = rf.get('/foo')
+    middleware = StaticFileMiddleware()
+    redirect = HttpResponsePermanentRedirect('/foo/')
+    response = middleware.process_response(request, redirect)
+    assert response == redirect
+
+
+@patch('barbeque.static.StaticFileMiddleware.process_response')
+def test_new_style_middleware(process_response_mock, rf, patch_settings):
+    request = rf.get('/static/test.jpg')
+    get_response_mock = Mock()
+    get_response_mock.return_value = HttpResponseNotFound()
+    middleware = StaticFileMiddleware(get_response=get_response_mock)
+    middleware(request)
+    get_response_mock.assert_called_with(request)
+    process_response_mock.assert_called_with(
+        request, get_response_mock.return_value)
+
+
+def test_with_client_hit(client, db, patch_settings):
+    response = client.get('/static/test.jpg')
+    assert response.status_code == 200
+
+
+def test_with_client_redirect(client, db, patch_settings):
+    response = client.get('/foo')
+    assert response.status_code == 301
+    assert response['Location'] == '/foo/'
+
+
+def test_with_client_query_params(client, db, patch_settings):
+    response = client.get('/static/test.jpg?v=1')
+    assert response.status_code == 200
+    assert response['Content-Type'] == 'image/jpeg'
