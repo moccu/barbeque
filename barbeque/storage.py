907e3cdecc1a1b3d790150385e289a9ee0c5845d
Magdalena Rother <magdalena.rother@moccu.com>
2016-10-18 18:42:18 +0200

    Don't customise url method in storage

--- barbeque/storage.py
+++ barbeque/storage.py
@@ -13,23 +13,6 @@ from django.utils.six import iteritems
 
 
 class CompactManifestStaticFilesStorage(ManifestStaticFilesStorage):
-    def url(self, name, force=False):
-        """
-        This implements some kind of a tricky hack to get the url for static
-        files right.
-
-        What we did is: we remove the url part "static" if it is the first part
-        of the passed filenmae. The reason for this is, that we have to insert
-        the "static" folder to get compass to work - compass walks through the
-        path in the filesystem, the storage fail here because "static" is outside
-        of our static storage. One could try to make it more generic by using
-        STATIC_URL but as we hardcoded "static" everywhere, we do it here too.
-        """
-        name_parts = name.split(os.sep)
-        if name_parts[0] == 'static':
-            name = os.sep.join(name_parts[1:])
-
-        return super(CompactManifestStaticFilesStorage, self).url(name, force=force)
 
     def post_process(self, paths, dry_run=False, **options):
         """
