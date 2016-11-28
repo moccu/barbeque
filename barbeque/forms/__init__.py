93fa252553936f2ceba5f0fc2c282bca1ebe5e35
Magdalena Rother <magdalena.rother@moccu.com>
2016-11-28 11:07:05 +0100

    Add deprecation warning when importing forms mixins through init

--- barbeque/forms/__init__.py
+++ barbeque/forms/__init__.py
@@ -1,1 +1,10 @@
+import warnings
+
 from .mixins import ItemLimitInlineMixin, FloppyformsLayoutMixin, PlaceholderFormMixin  # noqa
+
+
+message = """Importing mixins directly from barbeque.forms is deprecated.
+Use barbeque.forms.mixins instead."""
+
+warnings.simplefilter('always', DeprecationWarning)
+warnings.warn(message, DeprecationWarning)
