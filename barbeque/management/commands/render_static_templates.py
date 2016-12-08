d02c4609c3f73b8abdc3d2024196cf5e35651c0b
Stephan Jaekel <steph@rdev.info>
2016-12-08 10:47:54 +0100

    Add command to render templates to static files.

---
+++ barbeque/management/commands/render_static_templates.py
@@ -0,0 +1,55 @@
+import os
+
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+from django.template import RequestContext, TemplateDoesNotExist
+from django.test.client import RequestFactory
+from django.utils import translation
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Acrtivate language to ensure we get the translation right.
+        translation.activate(settings.LANGUAGE_CODE)
+
+        self.stdout.write('Rendering static templates..')
+        templates = getattr(settings, 'BARBEQUE_STATIC_TEMPLATES', {})
+        for filename, template_name in templates.items():
+            self.handle_item(filename, template_name)
+
+    def handle_item(self, filename, template_name):
+        self.stdout.write(
+            '> Rendering template "{0}" for "{1}"'.format(template_name, filename))
+
+        content = self.render_template(template_name)
+        if content is not None:
+            self.write_file(filename, content)
+
+    def render_template(self, template_name, context=None):
+        template_context = {}
+        template_context.update(getattr(settings, 'COMPRESS_OFFLINE_CONTEXT', {}))
+        template_context.update(context or {})
+
+        # We need a fake request to use the RequestContext, which is needed to
+        # apply all context processors (for menus, etc.)
+        request = RequestFactory(HTTP_HOST=settings.ALLOWED_HOSTS[0].strip('.')).get('/')
+        request.user = AnonymousUser()
+
+        try:
+            return render_to_string(template_name, RequestContext(request, template_context))
+        except TemplateDoesNotExist:
+            self.stdout.write(
+                '- Template "{0}" does not exist!'.format(template_name))
+
+    def write_file(self, name, content):
+        path = os.path.join(settings.STATIC_ROOT, name)
+        directory = os.path.dirname(path)
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+
+        self.stdout.write('- Writing file "{0}"'.format(path))
+        with open(path, 'w+') as file_obj:
+            file_obj.write(content)
--- barbeque/tests/test_management.py
+++ barbeque/tests/test_management.py
@@ -2,10 +2,12 @@ import os
 
 import pytest
 
-from barbeque.management.commands.create_error_pages import Command
+from barbeque.management.commands.create_error_pages import Command as ErrorpagesCommand
+from barbeque.management.commands.render_static_templates import Command as RenderCommand
 
 
-class TestCommand:
+class TestErrorpagesCommand:
+
     @pytest.fixture(autouse=True)
     def setup(self, settings):
         self.errors_dir = os.path.join(settings.STATIC_ROOT, 'errors')
@@ -16,11 +18,31 @@ class TestCommand:
     def test_command(self, settings):
         settings.BARBEQUE_ERRORPAGES_MAP = {'500': 'empty_template.html'}
 
-        Command().execute()
+        ErrorpagesCommand().execute()
         assert os.listdir(self.errors_dir) == ['500.html']
 
     def test_skip_missing_template(self, settings):
         settings.BARBEQUE_ERRORPAGES_MAP = {'500': 'empty_template.html', '503': '503.html'}
 
-        Command().execute()
+        ErrorpagesCommand().execute()
         assert os.listdir(self.errors_dir) == ['500.html']
+
+
+class TestRenderCommand:
+
+    def test_command(self, settings):
+        settings.BARBEQUE_STATIC_TEMPLATES = {'folder/robots.txt': 'empty_template.html'}
+
+        RenderCommand().execute()
+        assert 'robots.txt' in os.listdir(os.path.join(settings.STATIC_ROOT, 'folder'))
+
+    def test_skip_missing_template(self, settings):
+        settings.BARBEQUE_STATIC_TEMPLATES = {
+            'robots.txt': 'empty_template.html',
+            'nofolder/notfound.txt': 'no_template.html',
+            'folder/found.txt': 'empty_template.html',
+        }
+
+        RenderCommand().execute()
+        assert 'robots.txt' in os.listdir(settings.STATIC_ROOT)
+        assert 'found.txt' in os.listdir(os.path.join(settings.STATIC_ROOT, 'folder'))
